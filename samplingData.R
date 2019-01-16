ac35a0bed2746336ef109c2d25ba26195d2cab40
Brady Hoskins <bchoskins@sandhill.psychiatry.uiowa.edu>
2019-01-16 17:11:34 -0600

    Fixing Gender Bias

--- samplingData.R
+++ samplingData.R
@@ -1,4 +1,4 @@
-#Sample data/Imputation of missing affected data
+#Sample data/Imputation of missing affected data/construct models from sampled data
 
 data = read.delim2('goodData.csv', header = TRUE, sep = ",", dec = ",", stringsAsFactor = FALSE)
 data = data[c(-1)]
@@ -6,6 +6,13 @@ data = data[c(-1)]
 library(dplyr)
 #used later for imputing values (has missing data)
 affectedData <- select(filter(data, Affected == 1), c(1:65))
+
+#ratio for females to males in affected data
+#ratioAffected <- sum(affectedData$gender == "F")/sum(affectedData$gender == "M")
+#ratioAffected = 0.4081633
+
+#ratioMale <- sum(affectedData$gender =="M")/ nrow(affectedData)
+
 #use for getting control groups (will remove missing data below)
 nonAffectedData <- select(filter(data, Affected == 0), c(1:65))
 
@@ -15,11 +22,26 @@ library(plyr)
 nonAffectedData$gender <- revalue(nonAffectedData$gender, c("U"= NA))
 nonAffectedData$gender[nonAffectedData$gender==""] <- NA
 
-# library(dplyr) 
-#remove rows with missing values so we do not have to impute
-#add back in the affected data since it includes NAs so we can sample 
+
+# remove rows with missing values so we do not have to impute such a large dataset of controls
+# add back in the affected data since it includes NAs so we can pull control groups
+# imputation is done later with one control group (group1) and all affected observations
 # based on all NA ids
-# #sort by birth_year then lab_no
+# sort by birth_year then lab_no
+
+# newdata <- nonAffectedData[rowSums(is.na(nonAffectedData)) == 0,]
+#
+# justMale <- select(filter(newdata, gender == "M"), c(1:65))
+selectFemale <- newdata[ sample( which(newdata$gender=='F'), round(0.42*length(which(newdata$gender=='F')))), ]
+#
+# ratioNA <- nrow(selectFemale)/nrow(justMale)
+# #ratioNA = 0.4006221
+#
+# fixedData <- dplyr::union(justMale, selectFemale) %>%
+#           arrange(., birth_year, new_index)
+#
+# finalRatio<- sum(fixedData$gender =="F")/sum(fixedData$gender =="M")
+
 newdata <- nonAffectedData[rowSums(is.na(nonAffectedData)) == 0,] %>%
            dplyr::union(., affectedData) %>%
            arrange(., birth_year, new_index)
@@ -31,10 +53,42 @@ newdata$gender = as.factor(newdata$gender)
 
 newdata$Affected = as.factor(newdata$Affected)
 
-#Pull a 50 observation control group based on distance from each affected id (+-200)
-#and store into a list to use as a total control group
+#Pull two 50 observation control groups based on distance from each affected id (+-200)
+#and store into a list
 
-#while loop to make sure we pass KS Test
+#REVIEW HERE
+ks1 = 0
+ks1$p.value = 0
+while(ks1$p.value < 0.2) {
+  #print("here")
+  temp1 = vector()
+  for (i in affectedData$new_index) {
+    #print(i)
+    # y here is the row number of affected ids in the full dataset
+    y1 = which(newdata$new_index == i) 
+    #print(y1)
+    #temp1 = append(temp1, newdata[sample((y1-250):(y1+250) , 50, replace = FALSE), "new_index"])
+    temp1 = append(temp1, newdata[base::sample((y1-250):(y1+250), 50, replace = FALSE, 
+        prob = ((ifelse(newdata[(y1-250):(y1+250),'gender'] == "M", .66, .33))/sum(ifelse(newdata[(y1-250):(y1+250),'gender'] == "M", .66, .33)))), "new_index"])
+  }
+  temp1 <- sort(temp1)
+  control1 <- as.data.frame(temp1)
+  #make sure there are no duplicate control values
+  control1 <- unique(control1)
+  #make sure there are no affected ids within the control group
+  non_overlap1 <- dplyr::anti_join(control1, affectedData, by = c("temp1" = "new_index"))
+  
+  #K-S test
+  ks1 <- ks.test(non_overlap1, affectedData$new_index)
+  #want p-value < 0.2 to reject null hypothesis that distributions are not significantally different
+  print(ks1)
+}
+
+
+
+#while loop to make sure we pass K-S Test (testing for distribution distance between actual and control)
+#want to check that the distance between control distribution is significantly different than the actual distribution
+# this control group is used for imputing missing affected data
 #Control group 1
 ks1 = 0
 ks1$p.value = 0
@@ -55,16 +109,16 @@ while(ks1$p.value < 0.2) {
   #make sure there are no affected ids within the control group
   non_overlap1 <- dplyr::anti_join(control1, affectedData, by = c("temp1" = "new_index"))
   
-  #KS test
+  #K-S test
   ks1 <- ks.test(non_overlap1, affectedData$new_index)
-  #p-value = 1, fail to reject null hpyothesis that the two samples are not significantly different
+  #want p-value < 0.2 to reject null hypothesis that distributions are not significantally different
   print(ks1)
 }
 
 # this attaches all data because above just gets ids for sample 
 group1 <- select(filter(newdata, new_index %in% non_overlap1$temp), c(1:65))
 
-
+#repeate above for second control group used for modeling
 #Control Group 2
 ks2 = 0
 ks2$p.value = 0
@@ -85,20 +139,20 @@ while(ks2$p.value < 0.2) {
   #make sure there are no affected ids within the control group
   non_overlap2 <- dplyr::anti_join(control2, affectedData, by = c("temp2" = "new_index"))
   
-  #KS test
+  #K-S test
   ks2 <- ks.test(non_overlap2, affectedData$new_index)
-  #p-value = 1, fail to reject null hpyothesis that the two samples are not significantly different
+  #want p-value < 0.2 to reject null hypothesis that distributions are not significantally different
   print(ks2)
 }
 
 # this attaches all data because above just gets ids for sample 
 group2 <- select(filter(newdata, new_index %in% non_overlap2$temp), c(1:65))
 
-# make sure to have two different control groups
+# make sure to have two entirely different control groups
 library(dplyr)
 group2 <- anti_join(group1, group2, by = "new_index")
 
-#use group1 to impute Affected NAs then pull back out
+# use group1 to impute Affected NAs then pull affecteds back out
 # need to first get both datasets to have matching types
 affectedData[,c(5:65)]<- sapply(affectedData[,5:65],as.numeric)
 
@@ -124,10 +178,12 @@ library(VIM)
 #                   labels=names(imputeData), cex.axis=.7, gap=3,
 #                   ylab=c("Histogram of missing data","Pattern"))
 
-# impute data to fill missing values using 'cart' method to account for categorical variables
-# lso, attempted using 'rf' (random forest) method Note: default method 'pmm' does not work 
+# impute data to fill missing values using 'cart' method to account for categorical variables (*did not do, now using 'rf' method)
+# Also, attempted using 'rf' (random forest) method 
+# Note: default method 'pmm' does not work 
 # Info found at: https://www.rdocumentation.org/packages/mice/versions/3.3.0/topics/mice
 # Step-by-step: https://www.youtube.com/watch?v=zX-pacwVyvU
+
 # tempData <- mice(imputeData,m=5,maxit=5,meth='cart',seed=500)
 # logged errors on 6 variables (all observations too similar) 
 #BRINGS ISSUE 6 variables have too similar of data for every record in prediciton
@@ -174,27 +230,34 @@ pBack <- subset(imputeData, select = c(18, 23, 24, 28, 30, 47))
 #attach and reorder
 full <- cbind(completeDatarf, pBack) %>%
   .[, c(1:17, 60, 18:21, 61, 62, 22:24, 63, 25, 64, 26:41, 65, 42:59)]
+
 library(arsenal)
 #check that the variables are in correct order
 summary(compare(imputeData, full))
 
 # pull out affected data to have a full data set based on imputed data
-# put this full data back into entire dataset from earlier
+# put this full data back into entire dataset from earlier (Also, will
+# be used for modeling with control group 2)
 newAffected <- select(filter(full, Affected == 1), c(1:65))
 fullData <- select(filter(newdata, Affected == 0), c(1:65)) %>%
   dplyr::union(., newAffected) %>%
   arrange(., birth_year, new_index)
 
-# add affecteds back into group 2
+pract <- group2[sample()]
+
+# add imputed affecteds back into group 2
 modelData <- dplyr::union(group2, newAffected) %>%
   arrange(., birth_year, new_index)
 # fix warning by changing from character back to factor
-
 modelData$Affected <- as.factor(modelData$Affected)
+
 
 #******************
 #***GOOD TO HERE***
 #******************
+
+ratio <- sum(newAffected$gender == "F" ) / sum(newAffected$gender == "M")
+ratioControl <- sum(group2$gender == "F") / sum(group2$gender == "M")
 
 # Splitting the data into training set and test set
 library(caTools)
@@ -203,16 +266,20 @@ split = sample.split(modelData$Affected, SplitRatio = 0.75)
 training_set = subset(modelData, split == TRUE)
 test_set = subset(modelData, split == FALSE)
 
-# Feature scaling (since things like birth_year and new_index vary 
-# greatly from other continuous variables) ???????? 
+#***Feature scaling (since things like birth_year and new_index vary 
+# greatly from other continuous variables)*** ???????? 
 training_set[,c(1,3,5:65)] = scale(training_set[,c(1,3,5:65)])
 test_set[,c(1,3,5:65)] = scale(test_set[,c(1,3,5:65)])
 
 #Fitting Logistic Regression to the Training Set
-# classifier = glm(formula = Affected ~ .,
-#                  family = "binomial",
-#                  data = training_set,
-#                  maxit = 50)
+classifier = glm(formula = Affected ~ .,
+                 family = "binomial",
+                 data = training_set,
+                 maxit = 50)
+
+summary(classifier)
+plogis(predict(classifier, test_set, type = "response"))
+predict(classifier, test_set)
 
 # library(logistf)
 # classifier = logistf(Affected ~ ., data = training_set)
@@ -232,6 +299,8 @@ classifier.bayesglm = bayesglm(Affected ~ ., family = binomial(link="logit"),
 
 summary(classifier.bayesglm)
 
+
+
 # Predicting test set results 
 prob_pred = predict(classifier.bayesglm, type = 'response', newdata = test_set[-4])
 y_pred = ifelse(prob_pred > 0.5, 1, 0)
@@ -240,89 +309,5 @@ y_pred = ifelse(prob_pred > 0.5, 1, 0)
 # pretty poor classifier but we'll try better later
 cm = table(test_set[,4], y_pred)
 
-
-
-
-
-
-
-
-
-
-
-
-#****BELOW HERE IS DATA CAMP LOGISTIC REGRESION****
-
-# library(rsample)
-# set.seed(42)
-# 
-# #prepare the intial split object
-# data_split <- initial_split(modelData, prop=0.75)
-# # Extract the training dataframe 
-# training_data <- training(data_split)
-# # Extract the testing data frame
-# testing_data <- testing(data_split)
-# 
-# set.seed(42)
-# cv_split <- vfold_cv(training_data, v =5)
-# 
-# library(tidyverse)
-# cv_data <- cv_split %>%
-#   mutate(
-#     train = map(splits, ~training(.x)),
-#     validate = map(splits, ~testing(.x))
-#   )
-# 
-# # Build a model using the train data for each fold 
-# # of the cross validation
-# cv_models_lr <- cv_data %>%
-#   mutate(model = map(train, ~glm(formula = Affected~., data = .x, 
-#                                  family = "binomial")))
-# 
-# # Extract the first model and validate 
-# model <- cv_models_lr$model[[1]]
-# validate <- cv_models_lr$validate[[1]]
-# 
-# # Prepare binary vector of actual Attrition values in validate
-# validate_actual <- validate$Affected == "Yes"
-# 
-# # Predict the probabilities for the observations in validate
-# validate_prob <- predict(model, validate, type = "response")
-# 
-# # Prepare binary vector of predicted Attrition values for validate
-# validate_predicted <- validate_prob > 0.5
-# 
-# library(Metrics)
-# 
-# # Compare the actual & predicted performance visually using a table
-# table(validate_actual, validate_predicted)
-# 
-# # Calculate the accuracy
-# accuracy(validate_actual, validate_predicted)
-# 
-# # Calculate the precision
-# precision(validate_actual, validate_predicted)
-# 
-# # Calculate the recall
-# recall(validate_actual, validate_predicted)
-# 
-# cv_prep_lr <- cv_models_lr %>% 
-#   mutate(
-#     # Prepare binary vector of actual Attrition values in validate
-#     validate_actual = map(validate, ~.x$Affected == "Yes"),
-#     # Prepare binary vector of predicted Attrition values for validate
-#     validate_predicted = map2(.x = model, .y = validate, ~predict(.x, .y, type = "response") > 0.5)
-#   )
-# 
-# # Calculate the validate recall for each cross validation fold
-# cv_perf_recall <- cv_prep_lr %>% 
-#   mutate(validate_recall = map2_dbl(validate_actual, validate_predicted, 
-#                                     ~recall(actual = .x, predicted = .y)))
-# 
-# # Print the validate_recall column
-# cv_perf_recall$validate_recall
-# 
-# # Calculate the average of the validate_recall column
-# mean(cv_perf_recall$validate_recall)
 
 
